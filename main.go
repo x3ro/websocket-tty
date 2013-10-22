002febbd1c4cdb5c59bb3593175559423727cecc
Lucas Jenss <lucas@x3ro.de>
2013-10-22 13:30:18 +0200

    Refactor working directory changing

--- main.go
+++ main.go
@@ -27,38 +27,47 @@ func execHandler(ws *websocket.Conn) {
         log.Fatal(err)
     }
 
-    log.Println(data)
+    log.Println("Received request", data)
 
     var out []byte
     if len(data.Cmd) > 2 && data.Cmd[0:3] == "cd " {
-        wd, wderr := os.Getwd()
-        if wderr != nil {
-            log.Fatal(err)
-        }
-
-        newwd := path.Clean(wd + "/" + data.Cmd[3:])
-
-        log.Println("changing directory to ", newwd)
-        err = os.Chdir(newwd)
-        if err != nil {
-            log.Println(err)
-        } else {
-            out = []byte("Changed directory to " + newwd)
-        }
+        out, err = changeWorkingDirectory(data.Cmd[3:])
     } else {
-        out, err = exec.Command("bash", []string{"-c", data.Cmd}...).CombinedOutput()
+        var cmd = exec.Command("bash", []string{"-c", data.Cmd}...)
+        out, err = cmd.CombinedOutput()
     }
 
     var error = ""
     if err != nil {
-        log.Println(err)
+        log.Println("Error ocurred executing command", err)
         error = err.Error()
     }
 
     err = websocket.JSON.Send(ws, Response{Result:string(out), Error:error})
     if err != nil {
-        log.Fatal(err)
+        log.Fatal("Could not send response", err)
     }
+}
+
+func changeWorkingDirectory(newPath string) (out []byte, err error) {
+    wd, wderr := os.Getwd()
+    if wderr != nil {
+        // No idea in which cases this happens, so not really sure
+        // how to recover from it.
+        log.Fatal("Could not get current working directory", err)
+    }
+
+    newWd := path.Clean(wd + "/" + newPath)
+
+    log.Println("Changing directory to", newWd)
+    err = os.Chdir(newWd)
+    if err != nil {
+        log.Println("Error changing directory", err)
+    } else {
+        out = []byte("Changed directory to" + newWd)
+    }
+
+    return
 }
 
 func main() {
@@ -81,6 +90,6 @@ func main() {
     http.Handle("/exec", websocket.Handler(execHandler))
     err = http.ListenAndServe("127.0.0.1:8080", nil)
     if err != nil {
-        log.Fatal("ListenAndServe: " + err.Error())
+        log.Fatal("ListenAndServe:" + err.Error())
     }
 }
